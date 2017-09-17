f5edd4cd23de8c7eb357c88b923985040e5b3a93
Julian Motz <me@julianmotz.com>
2017-09-17 11:07:17 +0200

    Implement pressed checkbox effect

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -8,7 +8,38 @@ export default class Checkbox extends FormComponent {
       context.querySelector('.checkbox__input')
     );
     super.init();
+    this.minAnimationDuration = 500;
+    this.animationPassed = false;
+    this.initEvents();
     console.debug('Checkbox initialized');
+  }
+
+  initEvents() {
+    this.context.addEventListener('mousedown', () => {
+      if (this.field.checked) {
+        return;
+      }
+      this.animationPassed = false;
+      setTimeout(() => {
+        this.animationPassed = true;
+      }, this.minAnimationDuration);
+      this.context.classList.add('is-pressed');
+    });
+    this.context.addEventListener('mouseup', () => {
+      if (this.field.checked) {
+        return;
+      }
+      if (!this.animationPassed) {
+        const itvl = setInterval(() => {
+          if (this.animationPassed) {
+            clearInterval(itvl);
+            this.context.classList.remove('is-pressed');
+          }
+        }, 50);
+      } else {
+        this.context.classList.remove('is-pressed');
+      }
+    });
   }
 }
 
