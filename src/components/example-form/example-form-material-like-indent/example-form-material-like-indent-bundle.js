2f35f77f7bd4a1b87b327f3d068bf621f9c08f6a
Julian Motz <me@julianmotz.com>
2017-09-26 08:52:28 +0200

    Implement example form material like indent

---
+++ src/components/example-form/example-form-material-like-indent/example-form-material-like-indent-bundle.js
@@ -0,0 +1,6 @@
+import './example-form-material-like-indent.scss';
+import '../../checkbox/checkbox-material-like/checkbox-material-like-bundle';
+import '../../message/message-material-like/message-material-like-bundle';
+import '../../radio/radio-material-like/radio-material-like-bundle';
+import '../../select/select-material-like-indent/select-material-like-indent-bundle';
+import '../../text-field/text-field-material-like-indent/text-field-material-like-indent-bundle';
