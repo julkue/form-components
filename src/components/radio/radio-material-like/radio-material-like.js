3f0ab4c08576e2c75533eab751c94164243a7d79
Julian Motz <me@julianmotz.com>
2017-12-05 15:04:48 +0100

    Only re-position direct sibling errors of radios

--- src/components/radio/radio-material-like/radio-material-like.js
+++ src/components/radio/radio-material-like/radio-material-like.js
@@ -8,19 +8,21 @@ export class Radio extends FormComponent {
       context.querySelector('.radio__error'),
       options
     );
-    super.init();
     this.relocateOuterError();
+    super.init();
     console.debug('Radio initialized');
   }
 
   relocateOuterError() {
-    // Some CMS are handling multiple radios as one element, as you can
+    // Some CMS (D8) are handling multiple radios as one element, as you can
     // only choose one option. In this case the error message is generated
-    // after all radios. Make sure to relocate such error elements into
-    // the actual radio.
-    const error = this.context.parentElement.querySelector('.radio__error');
-    if (error) {
+    // after the radios (not inside a radio div). Make sure to relocate such
+    // error elements into the actual radio.
+    const error = this.context.nextElementSibling;
+    if (error && error.classList.contains('radio__error')) {
+      // move the outside generated error message into the component
       this.context.appendChild(error);
+      this.errorField = error;
     }
   }
 }
